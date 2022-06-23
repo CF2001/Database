367360337fcb2b4c6f1737682f1934fbcfaadd99
vmashp <84442053+VS-UA@users.noreply.github.com>
2022-06-23 09:39:52 +0100

    consultas

---
+++ interface/Consultas.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lv_Consultas = New System.Windows.Forms.ListView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lv_Consultas
+        '
+        Me.lv_Consultas.Location = New System.Drawing.Point(243, 143)
+        Me.lv_Consultas.Name = "lv_Consultas"
+        Me.lv_Consultas.Size = New System.Drawing.Size(284, 227)
+        Me.lv_Consultas.TabIndex = 0
+        Me.lv_Consultas.UseCompatibleStateImageBehavior = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(334, 59)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Consultas"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lv_Consultas)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lv_Consultas As ListView
+    Friend WithEvents Label1 As Label
+End Class
---
+++ interface/Consultas.vb
@@ -0,0 +1,104 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Consultas
+    Dim CN As SqlConnection
+    Dim CMD As SqlCommand
+    Dim currentConsulta As Integer = 0
+    Private Sub InfoConsultas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CN = New SqlConnection("data source=LAPTOP-4HV6V7EN\SQLEXPRESS;integrated security=True;initial catalog=Hospital_Management_System")
+        CMD = New SqlCommand
+        CMD.Connection = CN
+    End Sub
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        lv_Consultas.HoverSelection = True
+        lv_Consultas.FullRowSelect = True
+        lv_Consultas.GridLines = True
+        lv_Consultas.ShowGroups = False
+
+
+        CMD.CommandType = CommandType.Text
+        CMD.CommandText = "SELECT * FROM Cirurgia"
+        CN.Open()
+        '' DataReader !! 
+        Dim RDR As SqlDataReader
+        RDR = CMD.ExecuteReader
+        lv_Consultas.Items.Clear()
+        While RDR.Read
+            Dim C As New Consulta
+
+            C.funcIDMedico = Convert.ToString(IIf(RDR.IsDBNull(RDR.GetOrdinal("funcIDMedico")), "", RDR.Item("funcIDMedico")))
+            C.dataConsulta = Convert.ToString(IIf(RDR.IsDBNull(RDR.GetOrdinal("dataConsulta")), "", RDR.Item("dataConsulta")))
+            C.hora = Convert.ToString(IIf(RDR.IsDBNull(RDR.GetOrdinal("hora")), "", RDR.Item("hora")))
+            C.noConsulta = Convert.ToString(IIf(RDR.IsDBNull(RDR.GetOrdinal("noConsulta")), "", RDR.Item("noConsulta")))
+            C.noUtenteSaude = Convert.ToString(IIf(RDR.IsDBNull(RDR.GetOrdinal("noUtenteSaude")), "", RDR.Item("noUtenteSaude")))
+
+            '' Add valores na listView
+            lv_Consultas.Items.Add(C.noUtenteSaude)   '' 1º coluna
+            lv_Consultas.Items(currentConsulta).SubItems.Add(C.noConsulta) '' 2º coluna
+            lv_Consultas.Items(currentConsulta).SubItems.Add(C.dataConsulta)
+            lv_Consultas.Items(currentConsulta).SubItems.Add(C.hora)
+            lv_Consultas.Items(currentConsulta).SubItems.Add(C.funcIDMedico)
+
+            currentConsulta += 1
+        End While
+        CN.Close()
+    End Sub
+End Class
+
+
+<Serializable()> Public Class Consulta
+    Private _funcIDMedico As String
+    Private _noConsulta As String
+    Private _hora As String
+    Private _dataConsulta As String
+    Private _noUtenteSaude As String
+
+    Property funcIDMedico As String
+        Get
+            Return _funcIDMedico
+        End Get
+        Set(ByVal value As String)
+            _funcIDMedico = value
+        End Set
+    End Property
+
+    Property noConsulta As String
+        Get
+            Return _noConsulta
+        End Get
+        Set(ByVal value As String)
+            _noConsulta = value
+        End Set
+    End Property
+
+    Property dataConsulta As String
+        Get
+            Return _dataConsulta
+        End Get
+        Set(ByVal value As String)
+            _dataConsulta = value
+        End Set
+    End Property
+
+    Property hora As String
+        Get
+            Return _hora
+        End Get
+        Set(ByVal value As String)
+            _hora = value
+        End Set
+    End Property
+
+
+    Property noUtenteSaude As String
+        Get
+            Return _noUtenteSaude
+        End Get
+        Set(ByVal value As String)
+            _noUtenteSaude = value
+        End Set
+    End Property
+
+End Class
+
