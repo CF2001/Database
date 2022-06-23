af0eced28c929b32b42689170d4b2b1730cc84c7
vmashp <84442053+VS-UA@users.noreply.github.com>
2022-06-23 07:50:29 +0100

    funcionarios sp + vb

--- SQL_scripts/stored_procedures.sql
+++ SQL_scripts/stored_procedures.sql
@@ -353,7 +353,186 @@ EXEC UpdateDadosPaciente_Internado  22222730, '2022-06-22', NULL;
 
 
 /* Inserir um Funcionário no sistema do Hospital  */
+DROP PROC sp_registoFuncionario;
+GO 
+CREATE PROCEDURE sp_registoFuncionario (@funcID INT, @primeiroNome CHAR(15), @ultimoNome CHAR(15), @dataNascimento DATE, @genero CHAR(1), @telefone CHAR(9), 
+									@morada CHAR(40), @salary SMALLMONEY, @email CHAR(30), @tipo CHAR(1), @deptID SMALLINT, @balcao CHAR(1))
+AS 
+BEGIN
 
+	DECLARE	@getID AS INT;
+
+	SELECT @getID = COUNT(*) FROM Funcionario WHERE @funcID = func_ID;
+
+	IF @getID = 1 
+		BEGIN
+			RAISERROR('ERRO: Já existe um funcionário com esse ID!', 16, 1) WITH LOG;
+		END
+	ELSE
+		IF @tipo = 'M'
+			BEGIN
+				INSERT INTO Funcionario VALUES  (@funcID, @primeiroNome, @ultimoNome, @dataNascimento, @genero, @telefone, @morada, @salary, @email, @tipo);
+				INSERT INTO Medico VALUES  (@funcID, @deptID);
+				PRINT 'Sucess';
+			END
+		ELSE IF @tipo = 'E'
+			BEGIN
+				INSERT INTO Funcionario VALUES  (@funcID, @primeiroNome, @ultimoNome, @dataNascimento, @genero, @telefone, @morada, @salary, @email, @tipo);
+				INSERT INTO Enfermeiro VALUES  (@funcID, @deptID);
+				PRINT 'Sucess';
+			END
+		ELSE
+			BEGIN
+				INSERT INTO Funcionario VALUES  (@funcID, @primeiroNome, @ultimoNome, @dataNascimento, @genero, @telefone, @morada, @salary, @email, @tipo);
+				INSERT INTO Rececionista VALUES  (@funcID, @balcao);
+				PRINT 'Sucess';
+			END
+END
 /* Eliminar um Funcionário do sistema em função do seu func_ID */
+DROP PROC sp_eliminarFuncionario;
+GO 
+CREATE PROCEDURE sp_eliminarFuncionario (@func_ID INT)
+AS 
+BEGIN
+		DECLARE @type AS CHAR(1);
+		DECLARE @medico_consulta AS INT;
+		DECLARE @medico_cirurgia AS INT;
+		DECLARE @enfermeiro_cirurgia AS INT;
+		DECLARE @equipa_cirurgia AS INT;
+		DECLARE @enfermeiro_supervisiona AS INT;
+		DECLARE @enfermeiro_auxilia AS INT;
 
+		SELECT @type = tipo FROM Funcionario WHERE @func_ID = func_ID;
+		SELECT @equipa_cirurgia = Cirurgia.EquipaM_ID FROM Cirurgia JOIN EM_contemMed ON Cirurgia.EquipaM_ID = EM_contemMed.EquipaM_ID WHERE @func_ID = func_ID_Medico;
+
+		IF @type = 'M'
+			BEGIN
+				-- Verificar se o medico tinha consulta marcada
+				SELECT @medico_consulta = COUNT (*) FROM Consulta WHERE func_ID_Medico = @func_ID;
+				IF @medico_consulta > 0	-- medico tem consulta
+				   UPDATE Consulta SET func_ID_Medico = NULL, noUtenteSaude = NULL, dataConsulta = NULL, hora = NULL WHERE func_ID_Medico = @func_ID;
+
+				-- Verificar se o medico tinha cirurgia marcada
+				SELECT @medico_cirurgia = COUNT (*) FROM Cirurgia JOIN EM_contemMed ON Cirurgia.EquipaM_ID = EM_contemMed.EquipaM_ID WHERE func_ID_Medico = @func_ID;
+				IF @medico_cirurgia > 0	-- medico tem cirurgia
+					UPDATE Cirurgia
+					SET tipoCirurgia = NULL, noUtenteSaude = NULL, dataCirurgia = NULL, Hora_Inicio = NULL, Duracao = NULL, EquipaM_ID = NULL, noBlocoOperatorio = NULL
+					WHERE EquipaM_ID = @equipa_cirurgia;
+
+				DELETE FROM Medico WHERE @func_ID = func_ID_Medico;
+				DELETE FROM Funcionario WHERE @func_ID = func_ID;			
+			END
+
+		ELSE IF @type = 'E'
+			BEGIN
+				--Verificar se o enfermeiro tinha cirurgia marcada
+				SELECT @enfermeiro_cirurgia = COUNT (*) FROM Cirurgia JOIN EM_contemEnf ON Cirurgia.EquipaM_ID = EM_contemEnf.EquipaM_ID WHERE func_ID_Enf = @func_ID;
+				IF @enfermeiro_cirurgia > 0	-- enfermeiro tem cirurgia
+					UPDATE Cirurgia
+					SET tipoCirurgia = NULL, noUtenteSaude = NULL, dataCirurgia = NULL, Hora_Inicio = NULL, Duracao = NULL, EquipaM_ID = NULL, noBlocoOperatorio = NULL
+					WHERE EquipaM_ID = @equipa_cirurgia;
+				/*
+				--Verificar se supervisiona alguma cama
+				SELECT @enfermeiro_supervisiona = COUNT (*) FROM Enf_Supervisiona JOIN Cama_Hospital ON Enf_Supervisiona.ID_Cama = Cama_Hospital.ID_Cama WHERE func_ID_Enf = @func_ID AND Cama_Hospital.camaLivre = 1;
+				IF @enfermeiro_supervisiona > 0 --enfermeiro supervisiona
+					UPDATE Enf_Supervisiona
+					SET func_ID_Enf = NULL, ID_Cama = NULL
+					WHERE func_ID_Enf = @func_ID;
+
+				--Verificar se auxilia algum paciente
+				SELECT @enfermeiro_auxilia = COUNT (*) FROM Enf_Auxilia WHERE func_ID_Enf = @func_ID;
+				IF @enfermeiro_auxilia > 0
+					UPDATE Enf_Auxilia
+					SET func_ID_Enf = NULL, noUtenteSaude = NULL
+					WHERE func_ID_Enf = @func_ID;
+				*/
+				DELETE FROM Enf_Supervisiona WHERE @func_ID = func_ID_Enf; --não tenho a certeza destes 2
+				DELETE FROM Enf_Auxilia WHERE @func_ID = func_ID_Enf;
+				DELETE FROM Enfermeiro WHERE @func_ID = func_ID_Enf;
+				DELETE FROM Funcionario WHERE @func_ID = func_ID;
+			END
+		ELSE
+			BEGIN
+				DELETE FROM Rececionista WHERE @func_ID = func_ID_Rec;
+				DELETE FROM Funcionario WHERE @func_ID = func_ID;
+			END
+		END
+END	
 /* Atualizar os dados de um Funcionário no Sistema.	*/
+DROP PROC sp_updateInfoFuncionario;
+GO 
+CREATE PROCEDURE sp_updateInfoFuncionario (@funcID INT, @primeiroNome CHAR(15), @ultimoNome CHAR(15), @dataNascimento DATE, @genero CHAR(1), @telefone CHAR(9), 
+									@morada CHAR(40), @salary SMALLMONEY, @email CHAR(30), @tipo CHAR(1))
+AS
+BEGIN 
+	BEGIN TRY
+		BEGIN TRANSACTION
+		SET NOCOUNT ON
+			DECLARE @funcID_old AS INT;
+			DECLARE @primeiroNome_old AS CHAR(15);
+			DECLARE @ultimoNome_old AS CHAR(15);
+			DECLARE @dataNascimento_old AS DATE;
+			DECLARE @genero_old AS CHAR(1);
+			DECLARE @telefone_old AS CHAR(9);
+			DECLARE @morada_old AS CHAR(40);
+			DECLARE @salary_old AS SMALLMONEY;
+			DECLARE @email_old AS CHAR(30);
+
+			SELECT @funcID_old = func_ID, @primeiroNome_old = primeiroNome, @ultimoNome_old = ultimoNome, @dataNascimento_old = dataNascimento, 
+			@genero_old = genero, @telefone_old = noTelefone, @morada_old = morada, @salary_old = salary, @email_old = email
+			FROM Funcionario
+			WHERE func_ID = @funcID;
+
+			IF @funcID != @funcID_old
+			BEGIN
+				UPDATE Funcionario SET func_ID = @funcID WHERE func_ID = @funcID_old;
+			END
+
+			IF @primeiroNome != @primeiroNome_old
+			BEGIN
+				UPDATE Funcionario SET primeiroNome = @primeiroNome WHERE func_ID = @funcID_old;
+			END
+
+			IF @ultimoNome != @ultimoNome_old
+			BEGIN
+				UPDATE Funcionario SET ultimoNome = @ultimoNome WHERE func_ID = @funcID_old;
+			END
+
+			IF @dataNascimento != @dataNascimento_old
+			BEGIN
+				UPDATE Funcionario SET dataNascimento = @dataNascimento WHERE func_ID = @funcID_old;
+			END
+
+			IF @genero != @genero_old
+			BEGIN
+				UPDATE Funcionario SET genero = @genero WHERE func_ID = @funcID_old;
+			END
+
+			IF @telefone != @telefone_old
+			BEGIN
+				UPDATE Funcionario SET noTelefone = @telefone WHERE func_ID = @funcID_old;
+			END
+
+			IF @morada != @morada_old
+			BEGIN
+				UPDATE Funcionario SET morada = @morada WHERE func_ID = @funcID_old;
+			END
+
+			IF @salary != @salary_old
+			BEGIN
+				UPDATE Funcionario SET salary = @salary WHERE func_ID = @funcID_old;
+			END
+
+			IF @email != @email_old
+			BEGIN
+				UPDATE Funcionario SET email = @email WHERE func_ID = @funcID_old;
+			END
+			
+			COMMIT TRANSACTION
+		END TRY
+
+		BEGIN CATCH
+			PRINT ERROR_MESSAGE()
+			ROLLBACK
+		END CATCH	
+END 
---
+++ interface/Funcionario.Designer.vb
@@ -0,0 +1,341 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Funcionario
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.txtFuncID = New System.Windows.Forms.TextBox()
+        Me.txtPrimeiroNome = New System.Windows.Forms.TextBox()
+        Me.txtUltimoNome = New System.Windows.Forms.TextBox()
+        Me.txtDataNascimento = New System.Windows.Forms.TextBox()
+        Me.txtGenero = New System.Windows.Forms.TextBox()
+        Me.txtMorada = New System.Windows.Forms.TextBox()
+        Me.txtTelefone = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.txtSalario = New System.Windows.Forms.TextBox()
+        Me.txtTipo = New System.Windows.Forms.TextBox()
+        Me.searchBar = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(241, 454)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 29)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Atualizar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(422, 454)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(94, 29)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Eliminar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(544, 370)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(94, 29)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Refresh"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(519, 84)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(94, 29)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Search"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(769, 118)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(127, 20)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "ID do Funcionario"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(979, 118)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(110, 20)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Primeiro Nome"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(769, 209)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(113, 20)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Segundo Nome"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(979, 209)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(145, 20)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Data de Nascimento"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(769, 279)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(57, 20)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Genero"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(979, 279)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(61, 20)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Morada"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(769, 370)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(66, 20)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "Telefone"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(979, 370)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(46, 20)
+        Me.Label8.TabIndex = 11
+        Me.Label8.Text = "Email"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(769, 458)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(55, 20)
+        Me.Label9.TabIndex = 12
+        Me.Label9.Text = "Salario"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(971, 458)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(39, 20)
+        Me.Label10.TabIndex = 13
+        Me.Label10.Text = "Tipo"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Location = New System.Drawing.Point(241, 144)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(275, 304)
+        Me.ListBox1.TabIndex = 14
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(241, 41)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(215, 20)
+        Me.Label11.TabIndex = 15
+        Me.Label11.Text = "Insira um ID de um Funcionário"
+        '
+        'txtFuncID
+        '
+        Me.txtFuncID.Location = New System.Drawing.Point(776, 159)
+        Me.txtFuncID.Name = "txtFuncID"
+        Me.txtFuncID.Size = New System.Drawing.Size(125, 27)
+        Me.txtFuncID.TabIndex = 16
+        '
+        'txtPrimeiroNome
+        '
+        Me.txtPrimeiroNome.Location = New System.Drawing.Point(971, 159)
+        Me.txtPrimeiroNome.Name = "txtPrimeiroNome"
+        Me.txtPrimeiroNome.Size = New System.Drawing.Size(125, 27)
+        Me.txtPrimeiroNome.TabIndex = 17
+        '
+        'txtUltimoNome
+        '
+        Me.txtUltimoNome.Location = New System.Drawing.Point(776, 232)
+        Me.txtUltimoNome.Name = "txtUltimoNome"
+        Me.txtUltimoNome.Size = New System.Drawing.Size(125, 27)
+        Me.txtUltimoNome.TabIndex = 18
+        '
+        'txtDataNascimento
+        '
+        Me.txtDataNascimento.Location = New System.Drawing.Point(971, 232)
+        Me.txtDataNascimento.Name = "txtDataNascimento"
+        Me.txtDataNascimento.Size = New System.Drawing.Size(125, 27)
+        Me.txtDataNascimento.TabIndex = 19
+        '
+        'txtGenero
+        '
+        Me.txtGenero.Location = New System.Drawing.Point(776, 312)
+        Me.txtGenero.Name = "txtGenero"
+        Me.txtGenero.Size = New System.Drawing.Size(69, 27)
+        Me.txtGenero.TabIndex = 20
+        '
+        'txtMorada
+        '
+        Me.txtMorada.Location = New System.Drawing.Point(901, 312)
+        Me.txtMorada.Name = "txtMorada"
+        Me.txtMorada.Size = New System.Drawing.Size(239, 27)
+        Me.txtMorada.TabIndex = 21
+        '
+        'txtTelefone
+        '
+        Me.txtTelefone.Location = New System.Drawing.Point(776, 403)
+        Me.txtTelefone.Name = "txtTelefone"
+        Me.txtTelefone.Size = New System.Drawing.Size(125, 27)
+        Me.txtTelefone.TabIndex = 22
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(943, 403)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(197, 27)
+        Me.txtEmail.TabIndex = 23
+        '
+        'txtSalario
+        '
+        Me.txtSalario.Location = New System.Drawing.Point(776, 496)
+        Me.txtSalario.Name = "txtSalario"
+        Me.txtSalario.Size = New System.Drawing.Size(125, 27)
+        Me.txtSalario.TabIndex = 24
+        '
+        'txtTipo
+        '
+        Me.txtTipo.Location = New System.Drawing.Point(971, 496)
+        Me.txtTipo.Name = "txtTipo"
+        Me.txtTipo.Size = New System.Drawing.Size(125, 27)
+        Me.txtTipo.TabIndex = 25
+        '
+        'searchBar
+        '
+        Me.searchBar.Location = New System.Drawing.Point(241, 84)
+        Me.searchBar.Name = "searchBar"
+        Me.searchBar.Size = New System.Drawing.Size(215, 27)
+        Me.searchBar.TabIndex = 26
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1182, 535)
+        Me.Controls.Add(Me.searchBar)
+        Me.Controls.Add(Me.txtTipo)
+        Me.Controls.Add(Me.txtSalario)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.txtTelefone)
+        Me.Controls.Add(Me.txtMorada)
+        Me.Controls.Add(Me.txtGenero)
+        Me.Controls.Add(Me.txtDataNascimento)
+        Me.Controls.Add(Me.txtUltimoNome)
+        Me.Controls.Add(Me.txtPrimeiroNome)
+        Me.Controls.Add(Me.txtFuncID)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents txtFuncID As TextBox
+    Friend WithEvents txtPrimeiroNome As TextBox
+    Friend WithEvents txtUltimoNome As TextBox
+    Friend WithEvents txtDataNascimento As TextBox
+    Friend WithEvents txtGenero As TextBox
+    Friend WithEvents txtMorada As TextBox
+    Friend WithEvents txtTelefone As TextBox
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents txtSalario As TextBox
+    Friend WithEvents txtTipo As TextBox
+    Friend WithEvents searchBar As TextBox
+End Class
